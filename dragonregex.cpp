09968d22e1c41906171a0e813e214e8bb99fe27e
TheCalpolMan <thecalpolman@gmail.com>
2026-08-11 15:46:12 +0100

    trying to get abseil and re to work via fetchcontent

--- dragonregex.cpp
+++ dragonregex.cpp
@@ -1,8 +1,8 @@
 #include "dragonregex.h"
 
-#include <regex>
 #include <sstream>
 #include <iostream>
+
 
 #include "information.h"
 
@@ -10,25 +10,13 @@ Dragon DragonRegex::ConstructDragon(const std::string &paste)
 {
     Dragon dragon;
 
-    std::regex name = std::regex("[^\\s]+\\s*\\(#\\d+\\)",
-        std::regex_constants::extended);
+    // std::regex name = std::regex("[^\\s]+\\s*\\(#\\d+\\)",
+    //     std::regex_constants::extended);
 
     std::string breedGenderString = "icon\\s*((Female)?(Male)?)\\s*(" + getConcatenatedDragonBreeds() + ")";
     // std::cout << paste << std::endl;
-    std::regex breedGender = std::regex(breedGenderString,
-        std::regex_constants::extended);
 
-    std::smatch matches;
-
-    bool matched = std::regex_search(paste, matches, name);
-
-    for (std::size_t i = 0; i < matches.size(); ++i)
-        std::cout << i << ": " << matches[i] << '\n';
-
-    matched = std::regex_search(paste, matches, breedGender);
-
-    for (std::size_t i = 0; i < matches.size(); ++i)
-        std::cout << i << ": " << matches[i] << '\n';
+    // re2::RE2::FullMatch("hello", "h.*o");
 
     std::cout << std::endl;
 
